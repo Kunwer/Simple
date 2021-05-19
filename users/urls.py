89fd67a0b4993412f40714636e2c50a324f0e6cb
Kunwer Naveesh <kunwernaveesh@gmail.com>
2021-05-20 01:29:35 +0530

    fixed audio muting issue and added link for sharing

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-
+from django.conf.urls import url
 urlpatterns = [
-    path('', views.home,name='home'),
+    url(r'^room', views.home,name='room-home'),
+    path('',views.home,name='home'),
+
 ]
